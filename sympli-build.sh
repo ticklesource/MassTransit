7ec14aa6d80156d208e2c7b72368231f54c97d8a
ktei <ktei2008@gmail.com>
2023-03-01 18:05:00 +1100

    fix build

--- sympli-build.sh
+++ sympli-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 PROJECTS="./src/MassTransit.Abstractions,./src/MassTransit,./src/Transports/MassTransit.AmazonSqsTransport"
 
 project_list=$(echo $PROJECTS | tr "," "\n")
@@ -8,5 +7,5 @@ project_list=$(echo $PROJECTS | tr "," "\n")
 for p in $project_list
 do
   dotnet restore --no-cache $p
-  dotnet build -p:Publish=enable $p -f netstandard2.1 --configuration Release
+  dotnet build -p:Publish=enable $p --configuration Release
 done
