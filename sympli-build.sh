3f3044a8264c9eadad7c3b5f36d9ba93ec38eb25
ktei <ktei2008@gmail.com>
2023-03-01 20:22:16 +1100

    fix build

--- sympli-build.sh
+++ sympli-build.sh
@@ -9,7 +9,7 @@ root=$PWD
 for p in $project_list
 do
   dotnet restore --no-cache $p
-  dotnet build $p -p:BUILD_NUMBER=$BUILD_NUMBER --configuration Release
+  dotnet build $p -p:BUILD_NUMBER=$BUILD_NUMBER -p:TargetFrameworks=netstandard2.1 --configuration Release
   dotnet pack $p -p:BUILD_NUMBER=$BUILD_NUMBER -p:TargetFrameworks=netstandard2.1 --no-build --configuration Release --output $p/nuget;
 
   # publish
