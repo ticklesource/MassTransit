81497745e62b9a05634c4e171004ded630d0be28
ktei <ktei2008@gmail.com>
2024-03-13 15:20:31 +1100

    8.2.0 RC

--- sympli-build-local.ps1
+++ sympli-build-local.ps1
@@ -6,10 +6,10 @@ $ErrorActionPreference = "Stop"
 
 $sso_profile = 'ticklestaging'
 $aws_region = 'ap-southeast-2'
-$version = '8.0.17'
-$build_number = '8'
+$version = '8.2.0'
+$build_number = '0'
 $nuget_dir = [IO.Path]::Combine($env:userprofile, "nuget_local")
-$prelease_tag = '-qblock'
+$prelease_tag = '-rc'
 
 $nuget_repo = aws ssm get-parameter --name "/DevOps/InternalNugetSource" --region $aws_region --output text --query Parameter.Value --profile $sso_profile
 $nuget_key = aws ssm get-parameter --name "/DevOps/NugetAPIKey" --with-decryption --region $aws_region --output text --query Parameter.Value --profile $sso_profile
@@ -30,8 +30,8 @@ if ($publish) {
         Break
     }
     Write-Output "Publishing local packages..."
-    $packages = "MassTransit.Abstractions,MassTransit,MassTransit.AmazonSQS"
-    foreach ($package_id in $packages.split(",")) {
+    $packages = "MassTransit.Abstractions, MassTransit, MassTransit.AmazonSQS"
+    foreach ($package_id in $packages.split(", ")) {
         $package_path = [IO.Path]::Combine($nuget_dir, "$package_id.$version.$build_number$prelease_tag.nupkg")
         dotnet nuget push $package_path --source $nuget_repo --api-key $nuget_key
     }
